5e345a7aec43d39289aa338f34207890e65a22d9
bucho <steelwind666@gmail.com>
2015-01-11 12:13:27 +0900

    import時にDemoが含まれないよう修正

--- gridwindow.py
+++ gridwindow.py
@@ -16,41 +16,41 @@ class GridWindow(object):
         (px, py), (x, y), (w, h) = self._position, pos, self._grid_size
         return (px + x * w, py + y * h)
 
-class GridWindowDemo(framework.Application):
-    POSITION = (0, 0)
-    GRID_SIZE = (10, 18)
-    LEFT  = (-1, 0)
-    DOWN  = ( 0, 1)
-    UP    = (0, -1)
-    RIGHT = (1,  0)
-    def __init__(self):
-        framework.Application.__init__(self)
-        self._window = None
-        self._character_pos = (1, 1)
-
-    def update(self):
-        down_keys =  self._keyboard.down_keys()
-        if not down_keys: return
-        key = down_keys[0]
-        if key == ord('h'): self._move(self.LEFT)
-        if key == ord('j'): self._move(self.DOWN)
-        if key == ord('k'): self._move(self.UP)
-        if key == ord('l'): self._move(self.RIGHT)
-        if key == ord('q'): sys.exit()
-
-    def _move(self, direction):
-        (x, y), (dx, dy) = self._character_pos, direction
-        self._character_pos = (x+dx, y+dy)
-
-    def set_screen(self, screen):
-        self._screen = screen
-        self._window = GridWindow(screen, self.POSITION, self.GRID_SIZE)
-
-    def draw(self):
-        self._screen.fill()
-        self._window.write(self._character_pos, '@', Color.LIME)
-
 if __name__ == '__main__':
+    class GridWindowDemo(framework.Application):
+        POSITION = (0, 0)
+        GRID_SIZE = (10, 18)
+        LEFT  = (-1, 0)
+        DOWN  = ( 0, 1)
+        UP    = (0, -1)
+        RIGHT = (1,  0)
+        def __init__(self):
+            framework.Application.__init__(self)
+            self._window = None
+            self._character_pos = (1, 1)
+
+        def update(self):
+            down_keys =  self._keyboard.down_keys()
+            if not down_keys: return
+            key = down_keys[0]
+            if key == ord('h'): self._move(self.LEFT)
+            if key == ord('j'): self._move(self.DOWN)
+            if key == ord('k'): self._move(self.UP)
+            if key == ord('l'): self._move(self.RIGHT)
+            if key == ord('q'): sys.exit()
+
+        def _move(self, direction):
+            (x, y), (dx, dy) = self._character_pos, direction
+            self._character_pos = (x+dx, y+dy)
+
+        def set_screen(self, screen):
+            self._screen = screen
+            self._window = GridWindow(screen, self.POSITION, self.GRID_SIZE)
+
+        def draw(self):
+            self._screen.fill()
+            self._window.write(self._character_pos, '@', Color.LIME)
+
     framework.GameRunner(GridWindowDemo())\
         .initialize_system()\
         .initialize_screen(640, 480, 16)\
