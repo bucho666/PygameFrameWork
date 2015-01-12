27d2adbec531be3ab5e6b67ccb397f5fee46366f
bucho <steelwind666@gmail.com>
2015-01-13 06:40:29 +0900

    各クラスを直接importできるよう、__init__.pyを更新

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from framework import Game
+from framework import GameRunner
+from gridwindow import GridWindow
+from tilesheet import AsciiTileSheet
+from coordinate import Coordinate
+from coordinate import Direction
+from color import Color
