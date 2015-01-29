20cb43d84783a858b4e5bdacd250c594e1abfff6
bucho <steelwind666@gmail.com>
2015-01-30 06:31:43 +0900

    scheduler.py: 使っていないクラスを削除

--- scheduler.py
+++ scheduler.py
@@ -49,49 +49,6 @@ class Schedule(object):
         def __call__(self):
             pass
 
-class Job(object):
-    def __init__(self, wait_frame):
-        self._wait_frame = wait_frame
-        Schedule.add(self)
-
-    def execute(self):
-        if self.is_ready(): return
-        self.action()
-        self.done()
-
-    def is_ready(self):
-        self._wait_frame -= 1
-        return self._wait_frame > 0
-
-    def done(self):
-        Schedule.remove(self)
-
-    def action(self):
-        pass
-
-class RepeatJob(Job):
-    def __init__(self, interval, repeat_max=0):
-        Job.__init__(self, interval)
-        self._interval = interval
-        self._repeat_max = repeat_max
-        self._repeat_count = 0
-
-    def done(self):
-        self._count_up()
-        if self._is_done(): Job.done(self)
-        else: self._repeat()
-
-    def _count_up(self):
-        if self._repeat_max:
-            self._repeat_count += 1
-
-    def _is_done(self):
-        return self._repeat_max and\
-            self._repeat_count == self._repeat_max
-
-    def _repeat(self):
-        self._wait_frame = + self._interval
-
 if __name__ == '__main__':
     import sys
     import time
